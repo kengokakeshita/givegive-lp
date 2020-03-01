4fa0ddc5c325461d407bee510457b2ef063b7a47
xakakeshita <k.kakeshita@xearts.jp>
2020-03-01 18:23:22 +0900

    修正点の解消

---
+++ js/index.js
@@ -0,0 +1,41 @@
+/*
+    スライダー
+================================*/
+
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 5000,
+    }
+});
+
+/*
+  ページトップのスムーススクロール
+================================*/
+
+$(function(){
+    $('a[href^="#top"]').click(function(){
+      var speed = 400;
+      var href= $(this).attr("href");
+      var target = $(href == "#top" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+});
+
+/*
+ スクロールした時の出現アニメーション
+================================*/
+
+// window.sr= new scrollReveal();
+// sr.reveal('.scroll-reveal');
